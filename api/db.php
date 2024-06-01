ccb9735fd92331b518343badef0eabdcb891f25a
mackliu <macklun@gmail.com>
2024-06-01 10:05:54 +0800

    第二次課程內容

--- api/db.php
+++ api/db.php
@@ -1,9 +1,9 @@
 <?php
 //建立資料庫連線所需參數
-$dsn="mysql:host=localhost;charset=utf8;dbname=db03";
+$dsn="mysql:host=localhost;charset=utf8;dbname=th_54_national";
 //$dsn="mysql:host=localhost;charset=utf8;dbname=th53j_National";
 //建立PDO物件
-$pdo=new PDO($dsn,'root','');
+$pdo=new PDO($dsn,'root','mack1007');
 
 //啟用session
 session_start();
